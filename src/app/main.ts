b2ad2c2e82c6b755d4800992357235713402119b
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-01-06 23:31:00 +0100

    Extender interfaces

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,9 +4,12 @@ addProduct({
     id: 1,
     title: 'product 1',
     createdAt: new Date(),
+    updatedAt: new Date(),
     stock: 90,
     category: {
       id: 12,
-      name: 'category 1'
+      name: 'category 1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
     }
 })
